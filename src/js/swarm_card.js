5076a92093cec6784d7e42647144f82edf4c18c6
Uwe Steinberg <uwe.steinberg@tristero.de>
2020-02-29 18:09:59 +0100

    Add filter and sort to swarm card

--- src/js/swarm_card.js
+++ src/js/swarm_card.js
@@ -3,10 +3,12 @@ import { createCard } from "./functions.js";
 
 const mainContainer = document.querySelector(".swarm__main__container");
 
-users.forEach(user => {
-  mainContainer.appendChild(createCard(user));
-});
-
-//console.log(card);
-
-//mainContainer.appendChild(card);
+// no fishes younger than 21
+const age = 21;
+let usersFilter = users.filter(user => user.age >= age);
+// young fishes first
+let usersSort = usersFilter.sort((user1, user2) => user1.age - user2.age);
+// create HTML code and hook it into the maincontainer div
+usersSort
+  .map(user => createCard(user))
+  .forEach(card => mainContainer.appendChild(card));

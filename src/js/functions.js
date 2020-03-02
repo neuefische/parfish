dce87df0a91d3b77bee8c0d44ef6d798257ab4fb
steinberggit <60884298+steinberggit@users.noreply.github.com>
2020-03-02 09:32:22 +0100

    Refactoring the functions.js and the swarm_card.js and add a setup.js (#25)
    * Add refactoring for functions.js and add file setup.js
    
    * Add filter and sort to swarm card
    
    * Add array methods map, filter and sort to swarm_card.js, change user_data.js and import variables to swarm_card.js
    
    * Add a slash to the dev entry in the .gitignore file

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,63 +1,86 @@
 export const createCard = user => {
+  const card = createSection(user);
+  const avatarImage = createAvatarImage(user);
+  const nickname = createNickname(user);
+
+  const profileContainer = createProfilContainer(user);
+  const profilBox1 = createProfileBox(user.age, user.distance + " km");
+  const profilBox2 = createProfileBox(user.relationship, user.biography);
+  profileContainer.appendChild(profilBox1);
+  profileContainer.appendChild(profilBox2);
+
+  const buttonBox = createButtonBox(user);
+  const buttonNo = createButtonNo(user);
+  const buttonYes = createButtonYes(user);
+  buttonBox.appendChild(buttonNo);
+  buttonBox.appendChild(buttonYes);
+
+  card.appendChild(avatarImage);
+  card.appendChild(nickname);
+  card.appendChild(profileContainer);
+  card.appendChild(buttonBox);
+
+  return card;
+
+  ////
+};
+
+const createSection = user => {
   const card = document.createElement("section");
   card.classList = "swarm__main__container__item";
-  ////
+  return card;
+};
+
+const createAvatarImage = user => {
   const avatarImage = document.createElement("img");
   avatarImage.src = `./assets/${user.avatar}`;
   avatarImage.alt = user.nickname;
-  ////
+  return avatarImage;
+};
+
+const createNickname = user => {
   const nickname = document.createElement("h1");
   nickname.classList = "swarm__main__container__item__nickname";
   const nicknameText = document.createTextNode(user.nickname);
   nickname.appendChild(nicknameText);
-  ////
+  return nickname;
+};
+
+const createProfilContainer = user => {
+  const profileContainer = document.createElement("div");
+  profileContainer.classList = "swarm__main__container__item__profile";
+  return profileContainer;
+};
+
+const createProfileBox = (text1, text2) => {
   const profileContainer = document.createElement("div");
   profileContainer.classList = "swarm__main__container__item__profile";
 
-  const profileBox1 = document.createElement("div");
-  profileBox1.classList = "swarm__main__container__item__profile__box";
-  const age = document.createElement("div");
-  age.classList =
+  const profileBox = document.createElement("div");
+  profileBox.classList = "swarm__main__container__item__profile__box";
+  const div1 = document.createElement("div");
+  div1.classList =
     "swarm__main__container__item__draught-age  swarm__main__container__item__data";
-  const ageText = document.createTextNode(user.age);
-  age.appendChild(ageText);
+  const div1Text = document.createTextNode(text1);
+  div1.appendChild(div1Text);
 
-  const distance = document.createElement("div");
-  distance.classList =
+  const div2 = document.createElement("div");
+  div2.classList =
     "swarm__main__container__item__draught-distance  swarm__main__container__item__data";
-  const distanceText = document.createTextNode(user.distance);
-  distance.appendChild(distanceText);
+  const div2Text = document.createTextNode(text2);
+  div2.appendChild(div2Text);
+  profileBox.appendChild(div1);
+  profileBox.appendChild(div2);
+  return profileBox;
+};
 
-  profileBox1.appendChild(age);
-  profileBox1.appendChild(distance);
-
-  //2. Box
-
-  const profileBox2 = document.createElement("div");
-  profileBox2.classList = "swarm__main__container__item__profile__box";
-  const relationship = document.createElement("div");
-  relationship.classList =
-    "swarm__main__container__item__draught-relationship swarm__main__container__item__data";
-  const relationshipText = document.createTextNode(user.relationship);
-  relationship.appendChild(relationshipText);
-
-  const bio = document.createElement("div");
-  bio.classList =
-    "swarm__main__container__item__draught-bio  swarm__main__container__item__data";
-  const bioText = document.createTextNode(user.biography);
-  bio.appendChild(bioText);
-
-  profileBox2.appendChild(relationship);
-  profileBox2.appendChild(bio);
-
-  profileContainer.appendChild(profileBox1);
-  profileContainer.appendChild(profileBox2);
-
-  ////
-
+const createButtonBox = user => {
   const buttonBox = document.createElement("div");
   buttonBox.classList = "swarm__main__container__item__button-box";
+  return buttonBox;
+};
 
+const createButtonNo = user => {
   const noBtn = document.createElement("button");
   noBtn.classList =
     "swarm__main__container__item__button swarm__main__container__item__button-no";
@@ -67,10 +90,10 @@ export const createCard = user => {
   noImg.alt = "No match button";
 
   noBtn.appendChild(noImg);
-  buttonBox.appendChild(noBtn);
+  return noBtn;
+};
 
-  //
-
+const createButtonYes = user => {
   const yesBtn = document.createElement("button");
   yesBtn.classList =
     "swarm__main__container__item__button swarm__main__container__item__button-yes";
@@ -80,15 +103,5 @@ export const createCard = user => {
   yesImg.alt = "Match button";
 
   yesBtn.appendChild(yesImg);
-  buttonBox.appendChild(yesBtn);
-
-  ////
-  card.appendChild(avatarImage);
-  card.appendChild(nickname);
-  card.appendChild(profileContainer);
-  card.appendChild(buttonBox);
-
-  return card;
-
-  ////
+  return yesBtn;
 };
